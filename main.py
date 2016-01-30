07198299d80e77ab3610e40e448e8978f8a202e9
Trofis <porto.thomas@gmail.com>
2016-01-30 03:29:01 +0100

    Cela fonctionne !

--- main.py
+++ main.py
@@ -2,73 +2,88 @@ import gs
 import gs.plus.render as render
 import gs.plus.input as input
 
-x =["@data/place_desert.png", '@data/place_espace.png', '@data/place_montagne.png','@data/chat.jpg','@data/phoque.jpg','@data/phoque2.jpg' ]
-y =['Il fait chaud !', 'Ohh il fait beau', 'On va faire du ski ?', 'Ohh le chattt', 'Trop mignon le phoque', 'ohh je préfère celui-ci']
+phases = [[{'img' : "@data/place_desert.png", 'phrase':'Il fait chaud !'}, {'img' : '@data/place_espace.png', 'phrase':'Ohh il fait beau'}, {'img' : '@data/place_montagne.png', 'phrase':'On va faire du ski ?'}],[
+        {'img' : '@data/chat.jpg', 'phrase':'Ohh le chattt'} , {'img' : '@data/phoque.jpg', 'phrase':'Trop mignon le phoque'}, {'img' : '@data/phoque2.jpg', 'phrase':'ohh je préfère celui-ci'}]]
 
 gs.LoadPlugins(gs.get_default_plugins_path())
-class main(object):
-    def __init__(self, images, phrases):
-        self.listeImage = images
-        self.phrases = phrases
-        self.ImgPhr = []
-        self.phase = 0
-        self.listeImageAffich = []
-        render.init(1920, 1080, "pkg.core")
-        gs.MountFileDriver(gs.StdFileDriver("assets"),'@data')
+render.init(1920, 1080, "pkg.core")
+gs.MountFileDriver(gs.StdFileDriver("assets"),'@data')
 
-    # def distriImages(self):
-    #     for i in range(3):
-    #         self.listeImageAffich = getImages() [(self.phase*3)+i]
-
-    def getImages(self):
-        return self.listeImage
-
-    def getCommentaire(self):
-        return self.phrases
-
-    def AssocieImgPhr(self):
-        for i in range(3):
-            print((self.phase*3)+i)
-            j = self.phase*3 + i
-            self.ImgPhr.append({'img': self.getImages()[j], 'phrase': self.getCommentaire()[j]})
-
-    def getImgPhr(self):
-        return self.ImgPhr
-
-    def afficheTexte(self):
-        render.text2(400, 540, "Cest rigolo python")
+def main ():
+    intro()
+    gourou = selection()
+    generation(gourou)
 
 
-    def afficheImage(self, x, y ,i, echelle):
-        render.image2d(x,y,echelle,self.getImgPhr() [i] ['img'])
+def intro():
+    while not input.key_press(gs.InputDevice.KeyEnter):
+        render.clear()
+        afficheTexte(500, 400,'Quel est ton gourou')
+        afficheTexte(500, 300,'Choisis le sens de ta vie')
+        render.flip()
+    render.flip()
+def getImgParPha(phases):
+    return len(phases[0])
 
+def getImg(phases, phase, phrase_courante):
+    return phases [phase] [phrase_courante] ['img']
 
-if __name__ == "__main__":
-    o = main(x,y)
-    o.AssocieImgPhr()
-    indexDecor = -1
+def getTxt(phases, phase, phrase_courante):
+    return phases [phase] [phrase_courante] ['phrase']
+
+def selection():
+    Gourou = []
+    indexDecor = 0
     indexEntrer = -1
     entrer = False
-    o.phase = 0
-    while not input.key_press(gs.InputDevice.KeyEscape):
-        render.clear()
-        if input.key_press(gs.InputDevice.KeyLeft):
-            indexDecor-=1
+    indexImg = {}
+    for phase in range(len(phases)):
 
-        if input.key_press(gs.InputDevice.KeyRight):
-            indexDecor +=1
-        if indexDecor >= 0 and indexDecor < len(o.getImgPhr()):
-            o.afficheImage(1000,300, indexDecor, 0.5)
+        while not input.key_press(gs.InputDevice.KeyTab):
+            render.clear()
+            if input.key_press(gs.InputDevice.KeyLeft) and indexDecor > 0:
+                indexDecor-=1
 
-        if input.key_press(gs.InputDevice.KeyEnter):
-            entrer = True
-            indexEntrer = indexDecor
-        if entrer:
-            o.phase += 1
-            for i in range (o.phase+1):
-                o.afficheImage(300,200, indexEntrer, 1)
+            if input.key_press(gs.InputDevice.KeyRight) and indexDecor < getImgParPha(phases)-1:
+                indexDecor +=1
 
-            o.ImgPhr = []
-            o.AssocieImgPhr()
-            entrer = False
+            if indexDecor >= 0 and indexDecor < getImgParPha(phases):
+                afficheImage(1200,300, phase, 0.5, indexDecor)
+                afficheTexte(1200,200, getTxt(phases, phase,indexDecor ))
+
+            if input.key_press(gs.InputDevice.KeyEnter):
+                entrer = True
+                indexEntrer = indexDecor
+                Gourou.append(getTxt(phases,phase, indexDecor))
+            if entrer:
+                indexImg [phase] = indexEntrer
+                entrer = False
+
+            if phase > 0:
+                print(indexImg)
+                for index in range(len(indexImg)):
+                    afficheImage(100+(600*(index)),400,index , 0.7, indexImg[index])
+
+            render.flip()
         render.flip()
+
+def afficheTexte(x, y, texte):
+        render.text2d(x, y, texte)
+
+def afficheImage(x, y ,phase, echelle, phraseCourante):
+        render.image2d(x,y,echelle,getImg(phases, phase, phraseCourante) )
+
+def generation(gourou):
+    while not input.key_press(gs.InputDevice.KeyEnter):
+        render.clear()
+        afficheTexte(1000, 500, 'Voyons désormais quel est votre Gourou ....')
+        render.flip()
+    render.flip()
+
+    while not input.key_press(gs.InputDevice.KeyEnter):
+        render.clear()
+        afficheTexte(1000, 500, 'lol')
+        render.flip()
+    render.flip()
+
+main()
